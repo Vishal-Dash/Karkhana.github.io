0582a1c880d603d5fa625b386dc1783fb693f9b8
Vishal-Dash <vishaldash1998@gmail.com>
2019-06-07 13:48:12 +0530

    login added

--- login.js
+++ login.js
@@ -1,7 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const admin = require('firebase-admin');
+var serviceAccount = require('karkhana-service.json');
+const functions = require('firebase-functions');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+admin.initializeApp(functions.config().firebase);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var db = admin.firestore();
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDpK0-9RZXzLbKqV08kWLzEn0pJGQ9BkKc",
+    authDomain: "karkhana-makerspace.firebaseapp.com",
+    databaseURL: "https://karkhana-makerspace.firebaseio.com",
+    projectId: "karkhana-makerspace",
+    storageBucket: "karkhana-makerspace.appspot.com",
+    messagingSenderId: "897330186132",
+    appId: "1:897330186132:web:c577e48bfbb6680e"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  var firestore = firebase.firestore();
+
+  const docRef = firestore.doc("user/logindata");
+  const uname = document.querySelector("uname");
+  const pass = document.querySelector("pass");
+  const sub = document.querySelector("signin");
+
+  sub.addEventListener("click", function(){
+    const emailtosave = uname.nodeValue;
+    docRef.set({
+        UserName : uname
+    }).then(function(){
+        console.log("Text saved");
+    }).catch(function(error){
+        console.log("Error : ", error);
+    }); 
+  })
